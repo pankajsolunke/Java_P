48c768887ff664920816b6f38c93ddf1e434ea88
Pankaj D <pankajsolunke8506@gmail.com>
2025-02-04 16:19:04 +0530

    add a TypeCasting program in java

---
+++ TypeCasting.java
@@ -0,0 +1,22 @@
+package p_W3;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+//		int mynumber = 976712322;
+//		double myno = mynumber;
+//		
+//		System.out.println("after int "+ mynumber);
+//		System.out.println("before double "+ myno);
+//		
+//		double abhino =  -2147483647;
+//		int ano = (int) abhino;
+//		
+//		System.out.println("after double "+ abhino);
+//		System.out.println("before int "+ ano);
+		
+		
+
+	}
+
+}
