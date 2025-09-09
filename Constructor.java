984637e04e67a99ae0027fbc786778daac3274c8
Pankaj Solunke <94754779+pankajsolunke@users.noreply.github.com>
2025-09-09 14:25:47 +0530

    Super and this or Overloaded constructor

--- Constructor.java
+++ Constructor.java
@@ -1,13 +1,13 @@
 package Corejava;
 
-class A1{
-	String name;
-	int rollno;
-	
-	A1(String name,int rollno){
-		this.name = name;
-		this.rollno = rollno;
-		System.out.println("Name: "+ name +"\nRollno: "+ rollno );
+abstract class A1{
+	A1(){
+		System.out.println(this.hashCode());
+	}
+}
+class A2 extends A1{
+	A2(){
+		System.out.println(this.hashCode());
 	}
 }
 public class Constructor {
@@ -21,6 +21,7 @@ public class Constructor {
 	  * A constructor is not inherited.
 	  * It is Overloaded (you can have multiple constructor with different parameters).
 	  * If you define at least one constructor,java does not create a default constructor.
+	  * We can take constructor in any java class including abstract class also but we can't take constructor inside interface.
 	   
 	Types of Constructors
  1).Default constructor (no arguments) Ex. --> A()
@@ -29,7 +30,8 @@ public class Constructor {
 	If we are not writing at least one constructor then compiler will generate default constructor.
 	It is always no argument constructor.
 	Default constructor contains only one line. super(); it is a no argument call to super class constructor.
-Note.The access modifier of the default constructor is same as class modifier.(this rule is applicable only for public and default access modifier)
+Note.The access modifier of the default constructor is same as class modifier.
+(this rule is applicable only for public and default access modifier)
 	  
  2).Parameterized constructor (with arguments)
     Ex. --> A(String name)	  
@@ -51,30 +53,65 @@ Note.The access modifier of the default constructor is same as class modifier.(t
 	2).The only applicable modifiers for the constructors are public, default, private, protected.
 	3).If we are using any other modifier we will get compile time error.
 
-
-
-
-*/
-	static int count =0;
+	super() vs this():
+	The 1st line inside every constructor should be either super() or this() if we are not writing anything compiler will always generate super().
+	Case 1:We have to take super() or this() only in the 1st line of constructor.If we are taking anywhere else we will get compile time error.
+	Test()
 	{
-		count++;
+		System.out.println("constructor");
+		super();
 	}
-	Constructor(){
-		
+	Case 2:We can use either super() or this() but not both simultaneously.
+	Test()
+	{
+		super();
+		this();
+	}
+	Case 3:We can use super() or this() only inside constructor. If we are using anywhere else we will get compile time error.
+	public void methodOne()
+	{
+		super();
+	}
+	
+	That is we can call a constructor directly from another constructor only.
+	 ----super(): --> 1.we should use only in 1st line
+	 |  --this(): --> 2.only one but not both 
+	 |	|         |-> 3.only inside constructor
+	 |	|-->Calling another constructor of same class.        
+	 |-->Calling super class constructor.
+	 	         
+	Overloaded Constructors:
+		A class can contain more than one constructor and all these constructors having the same name but
+		different arguments and hence these constructors are considered as overloaded constructors.
+*/
+//	static int count =0;
+//	{
+//		count++;
+//	}
+	Constructor(double d){
+		System.out.println("Double-argument constructor");
 	}
 	Constructor(int i){
-		
+		this(10.2);
+		System.out.println("Int-argument constructor");
+	}
+	Constructor(){
+		this(10);
+		System.out.println("no-argument constuctor");
 	}
 	public static void main(String[] args) {
 		Constructor c= new Constructor();
+		//double-argument constructor/int-argument constructor/no-argument constructor
 		Constructor c1 = new Constructor(10);
-		Constructor c2 = new Constructor();
-		Constructor c3 = new Constructor();
-		System.out.println(c.count);
-		System.out.println(c1.count);
-		System.out.println(c2.count);
-		
-		A1 a1 = new A1("Om",43);
+		//double-argument constructor/int-argument constructor
+		Constructor c2 = new Constructor(10.5);
+		//double-argument constructor
+//		System.out.println(c.count);
+//		System.out.println(c1.count);
+//		System.out.println(c2.count);
+//		
+		A2 a = new A2();
+		System.out.println(a.hashCode());
 
 		
 	}
